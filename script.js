77224a5a1f54d62d1afb492b755f71c67e97a3b7
husaab <126218851+husaab@users.noreply.github.com>
2023-08-10 00:41:19 -0400

    randomized quotes now, no same quotes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var apikey ="1PM86ivvoKYdiurkdcQQwlqzG6a6gSD8qWV7s9W0tTuavtINk7l1knqL"
     var backgroundcount=0;
     var authorcount=0;
+    quotechange();
 
     $("#newquote").click(function (event){
         event.preventDefault();
@@ -24,9 +25,10 @@ $(document).ready(function(){
             url: "https://api.pexels.com/v1/search?query=nature&per_page=80",
 
             success: function(data) {
+                const backgroundrandomizer =  Math.floor(Math.random() * data.photos.length)
                 console.log(data)
-                console.log(data.photos[backgroundcount].src.original)
-                console.log(data.photos[backgroundcount].avg_color)
+                console.log(data.photos[backgroundrandomizer].src.original)
+                console.log(data.photos[backgroundrandomizer].avg_color)
 
                 
 
@@ -36,10 +38,10 @@ $(document).ready(function(){
                     $("body").css("background-image", "url(" + data.photos[backgroundcount].src.original + ")");
                 });*/
             
-                $("body").css("background-image", "url(" + data.photos[backgroundcount].src.original + ")");
-                $(":root").get(0).style.setProperty("--coloruse", data.photos[backgroundcount].avg_color)
+                $("body").css("background-image", "url(" + data.photos[backgroundrandomizer].src.original + ")");
+                $(":root").get(0).style.setProperty("--coloruse", data.photos[backgroundrandomizer].avg_color)
                 
-                backgroundcount+=1
+                //backgroundcount+=1
             },
 
             error: function (error){
@@ -55,19 +57,20 @@ $(document).ready(function(){
             url: "https://philosophy-quotes-api.glitch.me/quotes",
 
             success: function(data) {
+                const quoterandomizer = Math.floor(Math.random() * data.length)
                 console.log(data)
-                console.log(data[authorcount].quote)
+                console.log(data[quoterandomizer].quote)
                 $('.textbox').animate({ opacity: 0 }, 500, function () {
                     $(this).animate({ opacity: 1 }, 500);
-                    $('#text').html(data[authorcount].quote);
+                    $('#text').html(data[quoterandomizer].quote);
                   });
                 //$("#text").text(data[authorcount].quote)
                 $('.author').animate({ opacity: 0 }, 500, function () {
                     $(this).animate({ opacity: 1 }, 500);
-                    $('#author').html("- " + data[authorcount].source);
+                    $('#author').html("- " + data[quoterandomizer].source);
                   });
                 
-                authorcount+=1
+                //authorcount+=1
             },
 
             error: function (error){
