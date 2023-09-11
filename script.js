5eb7fac4360a1e543c73acdcd83b5139d6edf466
husaab <husseinsaabsrs@gmail.com>
2023-09-11 18:18:53 -0400

    added copy to clip board button, copies quote and author for user. fixed twitter button, now button fully functional

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
     let imagesData=[];
     let quotesData=[];
 
+    navigator.clipboard.writeText("How strange and foolish is man. He loses his health in gaining wealth. Then, to regain his health he wastes his wealth. He ruins his present while worrying about his future, but weeps in the future by recalling his past. He lives as though death shall never come to him, but dies in a way as if he were never born. - Ali Ibn Abi Talib");
+
     quotesearch();
     imagesearch();
     
@@ -25,9 +27,25 @@ $(document).ready(function(){
             $(this).animate({ opacity: 1 }, 500);
             $('#author').html("- " + quotesData[quoterandomizer].source);
           });
+
+        $('#tweet-quote').attr(
+            'href',
+            'https://twitter.com/intent/tweet?hashtags=quotes&text=' +
+            encodeURIComponent('"' + quotesData[quoterandomizer].quote + '" ' + quotesData[quoterandomizer].source)
+        );
+
+        $("#copy-quote").click(function (event) {
+            event.preventDefault();
+            console.log("copy")
+
+            navigator.clipboard.writeText(quotesData[quoterandomizer].quote + " - " + quotesData[quoterandomizer].source);
+        
+        });
+
         
         $("body").css("background-image", "url(" + imagesData[backgroundrandomizer].src.original + ")");
         $(":root").get(0).style.setProperty("--coloruse", imagesData[backgroundrandomizer].avg_color)
+
         
 
         //sol 1: create div, z index -1, put image inside it, new click replaces img
